aa7ac686f6e98368808b72a3f049cf64130288a2
Umar565 <titaniumgamingr@gmail.com>
2024-11-19 23:00:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "9972455"))
 API_HASH = getenv("API_HASH", "5b1fd83b698e4e6670f3dcb053eecc06")
-BOT_TOKEN = getenv("BOT_TOKEN", "7581466670:AAGV8o3QknrlAVXZsjGAXf5pjXO-4Nxymcs")
+BOT_TOKEN = getenv("BOT_TOKEN", "7430047499:AAF5Et-Y2Zm1lAAp9hVM-_T3X8u0Vu5XslA")
 OWNER_ID = int(getenv("OWNER_ID", "6928637808"))
 MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://mainmusicbot:musicbotdatabase@cluster0.hbvhe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = int(getenv("LOG_GROUP", "-1002357764876"))
